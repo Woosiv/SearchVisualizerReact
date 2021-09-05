e8a99745264efa702177a43b2421e75f1eb3ab20
Woosiv <williamhsiehoct@gmail.com>
2021-09-05 13:41:09 -0700

    Refactored code to fix bugs

--- src/App.js
+++ src/App.js
@@ -1,75 +1,85 @@
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 // import reportWebVitals from './reportWebVitals';
 
-function Grid(props) {
-  const [value, changeValue] = useState(5);
-  const [start, changeStart] = useState(null);
-  const [end, changeEnd] = useState(null);
-  const [grid, changeGrid] = useState([]);
 
-  function handleChange(event) {
-    changeValue(event.target.value);    // console.log('value is', value)    // generateSquares(event.target.value, start, changeStart, end, changeEnd);
+function Grid() {
+  let startArray = new Array(5);
+  for(let i =0; i< 5; i++) {
+    startArray[i] = new Array(5).fill('inactive')
   }
 
-  useEffect(() => {
-    /* Handles the onClick for the square,
-    allowing its state to be toggled through
-    different states */
-    function squareClick(x, y, state) {
-      console.log(x,y,state);
-      if(state !== 'inactive') {
-        if(state === 'start') {
-          changeStart(null);
-        }
-        else if (state === 'end') {
-          changeEnd(null);
-        }
-        return 'inactive';
-      }
-      else if (!start) {
-        changeStart([x,y]);
-        return 'start';
-      }
-      else if (!end) {
-        changeEnd([x,y]);
-        return 'end';
-      }
-      return 'wall';
+  const [value, changeValue] = useState(5);
+  const [grid, updateGrid] = useState(startArray);
+  const [start, changeStart] = useState(null);
+  const [end, changeEnd] = useState(null);
+
+  function sliderChange(e) {
+    let val = parseInt(e.target.value)
+    changeValue(val);
+    let newGrid = new Array(val);
+    for (let i = 0; i < val; i++) {
+      newGrid[i] = new Array(val).fill('inactive');
     }
-    
-    let gridSquares = new Array(value);
-    for (let i = 0; i < value; i++) {
-      gridSquares[i] = new Array(value);
-      for (let j = 0; j < value; j++) {
-        gridSquares[i][j] = <Square 
-                              key = {`${i},${j}`}
-                              x={i}
-                              y={j}
-                              test='pepelaff'
-                              onClick={squareClick} />;
+    updateGrid(newGrid);
+  }
+
+  function squareClick(x, y) {
+    const temp = grid.slice();
+    console.table(temp);
+    let state = temp[x][y];
+    if (state !== 'inactive') {
+      if (state === 'start') {
+        changeStart(null);
       }
+      else if (state === 'end') {
+        changeEnd(null);
+      }
+      temp[x][y] = 'inactive';
     }
-    changeGrid(gridSquares);
-  }, [value, start, end]);
+    else if (!start) {
+      changeStart([x,y]);
+      temp[x][y] = 'start';
+    } 
+    else if (!end) {
+      changeEnd([x,y]);
+      temp[x][y] = 'end';
+    }
+    else {
+      temp[x][y] = 'wall';
+    }
+    updateGrid(temp);
+  }
 
   return (
     <>
-      <div id='grid' style={{ gridTemplateColumns: `repeat(${grid.length}, 1fr)`, gridTemplateRows: `repeat(${grid.length}, 1fr)` }}>
-        {grid}
+      <div id = 'grid' style={{ gridTemplateColumns: `repeat(${value}, 1fr)`, gridTemplateRows: `repeat(${value}, 1fr)` }}>
+        {grid.map((row, x) => {
+          // console.log(row);
+          return row.map((col, y) => {
+            return <Square 
+                    key = {`${x},${y}`}
+                    x = {x}
+                    y = {y}
+                    state = {col}
+                    onClick = {squareClick}/>
+          })
+        })}
       </div>
-      <input type='range' min={3} max={25} value={value} onChange={handleChange} />
+      <p>{value}</p>
+      <input type='range' min={3} max={25} value={value} onChange={sliderChange}></input> 
     </>
-  );
+  )
 }
 
+
+
 function Square(props) {
-  const [state, changeState] = useState("inactive");
 
   return (
-    <div className={state} onClick={() => {
-      changeState(props.onClick(props.x, props.y, state))
+    <div className={props.state} onClick={() => {
+      props.onClick(props.x, props.y);
     }}>
     </div>
   )
@@ -83,41 +93,4 @@ function App() {
   );
 }
 
-/* Code Graveyard 
-// function generateGrid(x) {
-  //   x = parseInt(x);
-  //   let grid = new Array(x);
-  //   for (let i = 0; i < x; i++) {
-  //     grid[i] = new Array(x).fill(0);
-  //   }
-  //   return grid;
-  // }
-  changeState(props.onClick(props.x, props.y, state));
-      // if (state !== 'inactive') {
-      //   changeState("inactive")
-      //   if (state === "start") {
-      //     // props.changeEnds([null, props.ends[1]]);
-      //   }
-      //   else if (state === "end") {
-      //     // props.changeEnds([props.ends[0], null]);
-      //   }
-      //   // props.grid[props.x][props.y] = 0;
-      // }
-      // else if (!props.ends[0]) {
-      //   changeState("start")
-      //   // props.changeEnds([[props.x, props.y], props.ends[1]]);
-      //   // props.grid[props.x][props.y] = 1;
-      // }
-      // else if (!props.ends[1] && state !== 'start') {
-      //   changeState("end")
-      //   // props.changeEnds([props.ends[0], [props.x, props.y]]);
-      //   // props.grid[props.x][props.y] = 2;
-      // }
-      // else {
-      //   changeState("wall")
-      //   // props.grid[props.x][props.y] = -1;
-      // }
-      // console.log('Grid')
-      // console.table(props.grid)
-*/
 export default App;
